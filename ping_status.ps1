d018cf5bfb0c2f5510273e799aae9fc6371563a7
xraplace <64149093+meanstackofdoom@users.noreply.github.com>
2023-09-18 20:12:37 +1000

    Update ping_status.ps1
    Signed-off-by: xraplace <64149093+meanstackofdoom@users.noreply.github.com>

--- ping_status.ps1
+++ ping_status.ps1
@@ -38,106 +38,313 @@ Make sure to secure and protect sensitive information, such as webhook URLs.
 
 #>
 
-$pingWebhookUrl = "YOUR TEAMS WEBHOOK"
+# Weather API URL for 3-day forecast and alerts
+$forecastAndAlertsApiUrl = ""
 
-$ipAddresses = @(
-    @{ Name = "SERVER1"; Address = "192.168.1.1"; Site = "OFFICE1" },
-    @{ Name = "SERVER1"; Address = "192.168.1.2"; Site = "OFFICE1" },
-    @{ Name = "SERVER1"; Address = "192.168.1.3"; Site = "OFFICE2" },
-    @{ Name = "SERVER1"; Address = "192.168.1.4"; Site = "OFFICE2" }
-   )
+# Astronomy API URL
+$astronomyApiUrl = ""
 
-$pingIntervalSeconds = 600
-$batchSize = 8
+# Teams webhook URL
+$teamsWebhookUrl = ""
 
-$offlineStatus = @{}
-$logFilePath = "ping_status_log.txt"
+# Initialize variables to store max and min temperatures and precipitation
+$dayMaxTemp = $null
+$dayMinTemp = $null
 
-# Initialize $sites dynamically from $ipAddresses
-$sites = $ipAddresses.Site | Sort-Object -Unique
+# Function to fetch and report the highest and lowest temperatures between 5 PM and 5 PM next day
+function GetHighestAndLowestTemperaturesBetween5PM {
+    $currentHour = Get-Date -Format "HH"
 
-while ($true) {
-    $pingMessages = @()  
+    # Check if the current hour is 17 (5 PM)
+    if ($currentHour -eq "17") {
+        # Calculate the date for 5 PM today
+        $currentDate = Get-Date
+        $today5PM = Get-Date -Year $currentDate.Year -Month $currentDate.Month -Day $currentDate.Day -Hour 17 -Minute 0 -Second 0
 
-    $currentDateTime = Get-Date  
+        # Calculate the date for 5 PM next day
+        $nextDay5PM = $today5PM.AddDays(1)
 
-    $formattedMessage = @"
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-    }
-    h2 {
-        text-align: center;
-    }
-    p {
-        margin: 5px;
-    }
-    h3 {
-        margin-top: 15px;
-        margin-bottom: 5px;
-    }
-</style>
-</head>
-<body>
-<h2>Ping Server Status Report</h2>
-<Br>
-<p>Date: $($currentDateTime.ToString("yyyy-MM-dd HH:mm:ss"))</p>
-<Br>
-"@
+        # Fetch weather data for the specified time frame
+        $response = Invoke-RestMethod -Uri $forecastAndAlertsApiUrl -Method Get
 
-    foreach ($site in $sites) {
-        $pingMessages += "<h3>$site</h3>"
-
-        foreach ($device in ($ipAddresses | Where-Object { $_.Site -eq $site })) {
-            $pingResult = Test-Connection -ComputerName $device.Address -Count 1 -ErrorAction SilentlyContinue
-            $status = if ($pingResult) { "Online" } else { "Offline" }
-            $pingMessage = "Device: $($device.Name) | IP: $($device.Address) | Status: $status"
-
-            if ($status -eq "Offline") {
-                # Calculate offline duration
-                if (-not $offlineStatus.ContainsKey($device.Name)) {
-                    $offlineStatus[$device.Name] = Get-Date
-                }
-                $offlineDuration = (Get-Date) - $offlineStatus[$device.Name]
-                $pingMessage += " | Offline Duration: $($offlineDuration.ToString())"
-            } else {
-                $offlineStatus.Remove($device.Name)
-                $responseTime = if ($pingResult) { $pingResult.ResponseTime } else { "N/A" }
-                $packetLoss = if ($pingResult -and $pingResult.PacketLoss) { $pingResult.PacketLoss } else { "0%" }
-                $pingMessage += " | Response Time: $responseTime ms | Packet Loss: $packetLoss"
-            }
-
-            $pingMessages += "<p>$pingMessage</p>"
-
-            # Save to the log file as a plain text line
-            $logLine = "$site,$($device.Name),$($device.Address),$status,$($offlineDuration.TotalSeconds),$responseTime,$packetLoss"
-            $logLine | Out-File -Append -FilePath $logFilePath
+        # Check for errors in the API response
+        if ($response.error) {
+            Write-Host "Error fetching weather data: $($response.error.message)"
+            return
         }
 
-        $pingMessages += "<br />"  # Add a <br> under each site instead of <hr>
+        # Extract relevant weather information for the specified time frame
+        $forecast = $response.forecast.forecastday[0].hour
+
+        # Initialize variables for the highest and lowest temperatures
+        $highestTemp = $null
+        $lowestTemp = $null
+
+        # Find the highest and lowest temperatures between 5 PM and 5 PM next day
+        foreach ($hourData in $forecast) {
+            $hour = [datetime]::ParseExact($hourData.time, "yyyy-MM-dd HH:mm", $null)
+
+            if ($hour -ge $today5PM -and $hour -lt $nextDay5PM) {
+                $tempCelsius = $hourData.temp_c
+
+                # Check for the highest temperature
+                if ($highestTemp -eq $null -or $tempCelsius -gt $highestTemp) {
+                    $highestTemp = $tempCelsius
+                }
+
+                # Check for the lowest temperature
+                if ($lowestTemp -eq $null -or $tempCelsius -lt $lowestTemp) {
+                    $lowestTemp = $tempCelsius
+                }
+            }
+        }
+
+        # Create a message for the highest and lowest temperatures
+        $tempReportMessage = @{
+            "@type" = "MessageCard"
+            "@context" = "http://schema.org/extensions"
+            "themeColor" = "0072C6"
+            "summary" = "Highest and Lowest Temperature Report (5pm-to-5pm)"
+            "sections" = @(
+                @{
+                    "activityTitle" = "Temperature Report (5pm-to-5pm)"
+                    "facts" = @(
+                        @{
+                            "name" = "Date Range"
+                            "value" = "$($today5PM.ToString('yyyy-MM-dd HH:mm')) to $($nextDay5PM.ToString('yyyy-MM-dd HH:mm'))"
+                        },
+                        @{
+                            "name" = "Highest Temperature"
+                            "value" = "$highestTemp°C"
+                        },
+                        @{
+                            "name" = "Lowest Temperature"
+                            "value" = "$lowestTemp°C"
+                        }
+                    )
+                }
+            )
+        }
+
+        # Post the highest and lowest temperature report to Teams
+        $jsonTempReport = $tempReportMessage | ConvertTo-Json -Depth 5
+        Invoke-RestMethod -Uri $teamsWebhookUrl -Method Post -Body $jsonTempReport -ContentType "application/json"
+    }
+}
+
+
+
+
+
+
+# Create an infinite loop to update weather every hour
+while ($true) {
+    # Fetch weather data for the hourly report (with 3-day forecast and alerts)
+    $response = Invoke-RestMethod -Uri $forecastAndAlertsApiUrl -Method Get
+
+    # Check for errors in the API response
+    if ($response.error) {
+        Write-Host "Error fetching weather data: $($response.error.message)"
+        Start-Sleep -Seconds 3600  # Retry after 1 hour
+        continue
     }
 
-    $formattedMessage += "$($pingMessages -join '')</body></html>"
+    # Extract relevant weather information
+    $location = $response.location
+    $current = $response.current
 
-    Write-Host $formattedMessage
+    # Fetch astronomy data
+    $astronomyResponse = Invoke-RestMethod -Uri $astronomyApiUrl -Method Get
+    $astronomy = $astronomyResponse.astronomy.astro
 
-    $messageBody = @{
-        "text" = $formattedMessage
-        "contentType" = "html"
+    # Create a message to post in Teams for the hourly report
+    $message = @{
+        "@type" = "MessageCard"
+        "@context" = "http://schema.org/extensions"
+        "themeColor" = "0072C6"
+        "summary" = "  Weather Update"
+        "sections" = @(
+            @{
+                "activityTitle" = "40min  Weather Updates"
+                "facts" = @(
+                    @{
+                        "name" = "Location"
+                        "value" = "$($location.name), $($location.region), $($location.country)"
+                    },
+                    @{
+                        "name" = "Weather Condition"
+                        "value" = "$($current.condition.text)"
+                    },
+                    @{
+                        "name" = "Local Time"
+                        "value" = (Get-Date -Date ([System.DateTimeOffset]::FromUnixTimeMilliseconds($location.localtime_epoch * 1000)).LocalDateTime).ToString('yyyy-MM-dd HH:mm')
+                    },
+                    @{
+                        "name" = "Temperature (Feels Like)"
+                        "value" = "$($current.feelslike_c)°C"
+                    },
+                    @{
+                        "name" = "Wind"
+                        "value" = "$($current.wind_kph) km/h at $($current.wind_degree)°"
+                    },
+                    @{
+                        "name" = "Precipitation"
+                        "value" = "$($current.precip_mm) mm"
+                    },
+                    @{
+                        "name" = "UV Index"
+                        "value" = "$($current.uv) $($current.uv_description)"
+                    },
+                    @{
+                        "name" = "Wind Gust"
+                        "value" = "$($current.gust_kph) km/h"
+                    },
+                    @{
+                        "name" = "Air Quality (CarbonMonoxide)"
+                        "value" = "$($current.air_quality.co) μg/m³"
+                    },
+                    @{
+                        "name" = "Air Quality (Ozone)"
+                        "value" = "$($current.air_quality.o3) μg/m³"
+                    },
+                    @{
+                        "name" = "Air Quality (NitrogenDioxide)"
+                        "value" = "$($current.air_quality.no2) μg/m³"
+                    },
+                    @{
+                        "name" = "Air Quality (SulphurD)"
+                        "value" = "$($current.air_quality.so2) μg/m³"
+                    },
+                    @{
+                        "name" = "Air Quality (ParticulateM2.5)"
+                        "value" = "$($current.air_quality.pm2_5) μg/m³"
+                    },
+                    @{
+                        "name" = "Air Quality (ParticulateM10)"
+                        "value" = "$($current.air_quality.pm10) μg/m³"
+                    },
+                    @{
+                        "name" = "Visibility"
+                        "value" = "$($current.vis_km) km"
+                    },
+                    @{
+                        "name" = "Sunrise"
+                        "value" = $astronomy.sunrise
+                    },
+                    @{
+                        "name" = "Sunset"
+                        "value" = $astronomy.sunset
+                    },
+                    @{
+                        "name" = "Moonrise"
+                        "value" = $astronomy.moonrise
+                    },
+                    @{
+                        "name" = "Moonset"
+                        "value" = $astronomy.moonset
+                    },
+                    @{
+                        "name" = "Moon Phase"
+                        "value" = $astronomy.moon_phase
+                    },
+                    @{
+                        "name" = "Moon Illumination"
+                        "value" = $astronomy.moon_illumination
+                    }
+                )
+            }
+        )
     }
 
-    # Invoke the Teams webhook
-    Invoke-RestMethod -Uri $pingWebhookUrl -Method Post -Headers @{
-        "Content-Type" = "application/json"
-    } -Body ($messageBody | ConvertTo-Json)
-
-    $countdown = $pingIntervalSeconds
-    while ($countdown -gt 0) {
-        Write-Host "Next ping update in $countdown seconds..."
-        Start-Sleep -Seconds 1
-        $countdown--
+    # Check if "will_it_rain" and "chance_of_rain" are available in the API response
+    if ($current.PSObject.Properties['will_it_rain'] -ne $null -and $current.PSObject.Properties['chance_of_rain'] -ne $null) {
+        $message["sections"] += @{
+            "activityTitle" = "Hourly Weather Update"
+            "facts" = @(
+                @{
+                    "name" = "Chance of Rain"
+                    "value" = "$($current.chance_of_rain)%"
+                },
+                @{
+                    "name" = "Will it Rain?"
+                    "value" = "$($current.will_it_rain)"
+                }
+            )
+        }
     }
+
+    $alertDetails = @{
+    "name" = "Alert"
+    "value" = "Headline: $($alert.headline)`r`nSeverity: $($alert.severity)`r`nUrgency: $($alert.urgency)`r`nCategory: $($alert.category)`r`nCertainty: $($alert.certainty)`r`nEvent: $($alert.event)`r`nNote: $($alert.note)`r`nEffective: $($alert.effective)`r`nExpires: $($alert.expires)`r`nDescription: $($alert.desc)`r`nInstruction: $($alert.instruction)"
+}
+
+if ($alerts) {
+    $alertsSection = @{
+        "activityTitle" = "Weather Alerts"
+        "facts" = @($alertDetails)
+    }
+
+    $message["sections"] += $alertsSection
+} else {
+    # If no alerts are available, add a "No Alerts" section
+    $message["sections"] += @{
+        "activityTitle" = "Weather Alerts"
+        "facts" = @(
+            @{
+                "name" = "Alert"
+                "value" = "No Severe Weather alerts for "
+            }
+        )
+    }
+}
+
+    # Get the 3-day forecast
+    $forecast = $response.forecast.forecastday
+    $forecastSection = @{
+        "activityTitle" = "3-Day Forecast for"
+        "facts" = @()
+    }
+
+    foreach ($day in $forecast) {
+        $forecastDetails = @{
+            "name" = $day.date
+            "value" = "Max Temp: $($day.day.maxtemp_c)°C ($($day.day.maxtemp_f)°F)`r`nMin Temp: $($day.day.mintemp_c)°C ($($day.day.mintemp_f)°F)`r`nCondition: $($day.day.condition.text)"
+        }
+        $forecastSection["facts"] += $forecastDetails
+    }
+
+    $message["sections"] += $forecastSection
+
+  
+
+
+
+    # Post the message to Teams
+    $jsonMessage = $message | ConvertTo-Json -Depth 5
+    Invoke-RestMethod -Uri $teamsWebhookUrl -Method Post -Body $jsonMessage -ContentType "application/json"
+
+    # Calculate the time until the next update (41 minutes and 39 seconds)
+$secondsUntilNextUpdate = 41 * 60 + 39  # 41 minutes and 39 seconds
+
+# Loop to continuously update the countdown
+while ($secondsUntilNextUpdate -gt 0) {
+    $minutes = [math]::floor($secondsUntilNextUpdate / 60)
+    $seconds = $secondsUntilNextUpdate % 60
+
+    # Display the countdown in the console, overwriting the previous line
+    Write-Host -NoNewline -ForegroundColor Yellow " Weather Script : Next update in $minutes minutes and $seconds seconds..."
+
+    # Sleep for 1 second
+    Start-Sleep -Seconds 1
+
+    # Decrement the remaining time
+    $secondsUntilNextUpdate--
+
+    # Move to a new line
+    Write-Host ""
+}
+
+# Print a message on a new line when the countdown reaches 0
+Write-Host ""  # Blank line
+Write-Host -ForegroundColor Green " Update in progress..."
 }
