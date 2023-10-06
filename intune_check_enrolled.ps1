05c80f7fa8817a244265c5840f4482c2b54f29dd
xraplace <64149093+meanstackofdoom@users.noreply.github.com>
2023-10-06 15:17:12 +1100

    Update intune_check_enrolled.ps1
    Signed-off-by: xraplace <64149093+meanstackofdoom@users.noreply.github.com>

--- intune_check_enrolled.ps1
+++ intune_check_enrolled.ps1
@@ -7,10 +7,9 @@ The script performs the following tasks:
 1. Checks for the presence of the "Microsoft.Graph.Intune" PowerShell module and installs it if missing.
 2. Establishes a connection to Microsoft Graph.
 3. Updates the Microsoft Graph environment schema to the beta version.
-4. Calculates a time threshold to determine if any devices were enrolled within a specified time frame (default is 6 hours).
-5. Queries Intune-managed devices that meet the enrollment criteria.
-6. Sends a message to a Microsoft Teams channel with details of the enrolled devices, if any.
-7. Repeats the monitoring process at regular intervals.
+4. Queries Intune-managed devices that meet the enrollment criteria.
+5. Sends a message to a Microsoft Teams channel with details of the enrolled devices, if any.
+6. Repeats the monitoring process at regular intervals.
 
 .NOTES
 - You should replace the webhook URLs with your actual Microsoft Teams webhook URLs.
@@ -26,127 +25,120 @@ See LICENSE in the project root for license information.
 
 #>
 
-$IntuneModule = Get-Module -Name "Microsoft.Graph.Intune" -ListAvailable
+do {
+    <#
+    .COPYRIGHT
+    Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+    See LICENSE in the project root for license information.
+    #>
+    ####################################################
+    $IntuneModule = Get-Module -Name "Microsoft.Graph.Intune" -ListAvailable
 
-if (!$IntuneModule) {
-    Write-Host "Microsoft.Graph.Intune Powershell module not installed..." -f Red
-    Write-Host "Install by running 'Install-Module Microsoft.Graph.Intune' from an elevated PowerShell prompt" -f Yellow
-    Write-Host "Script can't continue..." -f Red
+    if (!$IntuneModule) {
+        Write-Host "Microsoft.Graph.Intune Powershell module not installed..." -f Red
+        Write-Host "Install by running 'Install-Module Microsoft.Graph.Intune' from an elevated PowerShell prompt" -f Yellow
+        Write-Host "Script can't continue..." -f Red
+        Write-Host
+        exit
+    }
+
+    ####################################################
+
+    if (!(Connect-MSGraph)) {
+        Connect-MSGraph
+    }
+
+    ####################################################
+
+    Update-MSGraphEnvironment -SchemaVersion beta -Quiet
+
+    ####################################################
+
+    # Filter for the minimum number of minutes when the device enrolled into the Intune Service
+
+    # 360 = 6 hours
+    $minutes = 360
+
+    $minutesago = "{0:s}" -f (get-date).addminutes(0-$minutes) + "Z"
+
+    $CurrentTime = [System.DateTimeOffset]::Now
+
     Write-Host
-    exit
-}
+    write-host "Checking if any Intune Managed Device Enrolled Date is within or equal to $minutes minutes..." -f Yellow
+    Write-Host
+    write-host "Minutes Ago:" $minutesago -f Magenta
+    Write-Host
+
+    ####################################################
+
+    $Devices = Get-IntuneManagedDevice -Filter "enrolledDateTime ge $minutesago" | sort deviceName
+
+    $Devices = $Devices | ? { $_.managementAgent -ne "eas" }
 
 
+    if ($Devices) {
+        $DeviceCount = @($Devices).count
 
-if (!(Connect-MSGraph)) {
-    Connect-MSGraph
-}
-
-
-
-Update-MSGraphEnvironment -SchemaVersion beta -Quiet
-
-
-
-# 360 = 6 hours
-$minutes = 360
-
-$minutesago = "{0:s}" -f (get-date).addminutes(0-$minutes) + "Z"
-
-$CurrentTime = [System.DateTimeOffset]::Now
-
-Write-Host
-write-host "Checking if any Intune Managed Device Enrolled Date is within or equal to $minutes minutes..." -f Yellow
-Write-Host
-write-host "Minutes Ago:" $minutesago -f Magenta
-Write-Host
-
-$Devices = Get-IntuneManagedDevice -Filter "enrolledDateTime ge $minutesago" | sort deviceName
-
-$Devices = $Devices | ? { $_.managementAgent -ne "eas" }
-
-# If there are devices not synced in the past 30 days script continues
-
-if ($Devices) {
-    $DeviceCount = @($Devices).count
-
-    # Create the message content
-    $Message = @"
-    **Devices Enrolled in the Past $minutes Minutes:**
-    Total Devices: $DeviceCount
+        # Create the message content
+        $Message = @"
+        **Devices Enrolled in the Past $minutes Minutes:**
+        Total Devices: $DeviceCount
 
 "@
 
-    # Add device details to the message
-    $Message += @"
-    **Device Name:** $($Device.deviceName)
-    **Management State:** $($Device.managementState)
-    **Operating System:** $($Device.operatingSystem)
-    **Device Type:** $($Device.deviceType)
-    **Last Sync Date Time:** $($Device.lastSyncDateTime)
-    **Enrolled Date Time:** $($Device.enrolledDateTime)
-    **Jail Broken:** $($Device.jailBroken)
-    **Compliance State:** $($Device.complianceState)
-    **AAD Registered:** $($Device.aadRegistered)
-    **Management Agent:** $($Device.managementAgent)
-    **Date Time difference is:** $TotalMinutes minutes from current date time
+        # Add device details to the message
+        foreach ($Device in $Devices) {
+            $Message += @"
+            **Device Name:** $($Device.deviceName)
+            **Management State:** $($Device.managementState)
+            **Operating System:** $($Device.operatingSystem)
+            **Device Type:** $($Device.deviceType)
+            **Last Sync Date Time:** $($Device.lastSyncDateTime)
+            **Enrolled Date Time:** $($Device.enrolledDateTime)
+            **Jail Broken:** $($Device.jailBroken)
+            **Compliance State:** $($Device.complianceState)
+            **AAD Registered:** $($Device.aadRegistered)
+            **Management Agent:** $($Device.managementAgent)
+            **Date Time difference is:** $TotalMinutes minutes from the current date time
 "@
+        }
 
+        # Replace this with your actual Teams webhook URL
+        $WebhookUrl = ""
 
+        # Send the message to Teams using the webhook
+        $Headers = @{
+            "Content-Type" = "application/json"
+        }
 
-    # Replace this with your actual Teams webhook URL
-    $WebhookUrl = "your_custom_webhook_from_teams"
+        $Body = @{
+            text = $Message
+        } | ConvertTo-JSon
 
-    # Send the message to Teams using the webhook
-$Headers = @{
-    "Content-Type" = "application/json"
-}
+        Invoke-RestMethod -Uri $WebhookUrl -Method Post -Headers $Headers -Body $Body
+    } else {
+        # If no devices enrolled in the past $minutes minutes, create a message
+        $Message = "There have been no devices enrolled or checked into AVS in the last $minutes minutes found,  when I find a device I will print out details here."
 
-$Body = @{
-    text = $Message
-} | ConvertTo-Json
+        # Replace this with your actual Teams webhook URL
+        $WebhookUrl = ""
 
-Invoke-RestMethod -Uri $WebhookUrl -Method Post -Headers $Headers -Body $Body
-} # Add this closing curly brace
+        # Send the message to Teams using the webhook
+        $Headers = @{
+            "Content-Type" = "application/json"
+        }
 
-else {
-    # If no devices enrolled in the past $minutes minutes, create a message
-    $Message = "There have been no devices enrolled or checked into business in the last $minutes minutes found,  when I find a device I will print out details here."
+        $Body = @{
+            text = $Message
+        } | ConvertTo-Json
 
-    # Replace this with your actual Teams webhook URL
-    $WebhookUrl = "your_custom_webhook_from_teams"
-
-    # Send the message to Teams using the webhook
-    $Headers = @{
-        "Content-Type" = "application/json"
+        Invoke-RestMethod -Uri $WebhookUrl -Method Post -Headers $Headers -Body $Body
     }
 
-    $Body = @{
-        text = $Message
-    } | ConvertTo-Json
+    # Define the interval in seconds (6 hours)
+    $IntervalInSeconds = 6 * 60 * 60
 
-    Invoke-RestMethod -Uri $WebhookUrl -Method Post -Headers $Headers -Body $Body
-}
-
-# Define the interval in seconds (6 hours)
-$IntervalInSeconds = 6 * 60 * 60
-
-# Loop indefinitely
-while ($true) {
-    $RemainingTime = $IntervalInSeconds
-    while ($RemainingTime -gt 0) {
-        $Hours = [math]::floor($RemainingTime / 3600)
-        $Minutes = [math]::floor(($RemainingTime % 3600) / 60)
-        $Seconds = $RemainingTime % 60
-
-        Write-Host "Check Enrolment: Next update in: $Hours hours, $Minutes minutes, $Seconds seconds" -ForegroundColor Cyan
-        Start-Sleep -Seconds 1  # Sleep for 1 second
-        $RemainingTime -= 1
-    }
-
-    Write-Host "Running Intune check..." -ForegroundColor Yellow
-    Run-IntuneCheck  # Run your Intune check function
-
+    # Sleep for the defined interval
     Write-Host "Update complete. Sleeping for 6 hours..." -ForegroundColor Green
     Start-Sleep -Seconds $IntervalInSeconds
-}
+} while ($true)
